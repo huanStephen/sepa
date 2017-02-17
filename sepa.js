a8221b2dce51b57b7b611a2cb3fce7d79f58027b
huanStephen <huanStephen@163.com>
2017-02-17 13:06:06 +0800

    有限状态机开发并测试

--- sepa.js
+++ sepa.js
@@ -5,7 +5,7 @@
  * Author:  huanStephen
  * License: GPL-3.0
  * Date:    2017-1-12
- * Update:  2017-2-4
+ * Update:  2017-2-9
  */
 (function($) {
 
@@ -36,6 +36,18 @@
         }
 
         return result;
+    };
+
+    /**
+     * Guid generator
+     * guid生成
+     * @returns {string}
+     */
+    Math.guid = function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        }).toUpperCase();
     };
 
     /**
